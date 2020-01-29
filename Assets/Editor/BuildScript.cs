a8c41506a6668f036b4ec3f64d91df0e920a5c4e
mpinol <michaelp@unity3d.com>
2020-01-29 14:36:48 -0800

    Adding build script.

---
+++ Assets/Editor/BuildScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public class BuildScript : MonoBehaviour
+{
+
+    public static void Build()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Roll-a-ball.unity" };
+        buildPlayerOptions.target = BuildTarget.StandaloneLinuxUniversal;
+        buildPlayerOptions.locationPathName = "../build/linux_rollaball_build";
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+    }
+}
